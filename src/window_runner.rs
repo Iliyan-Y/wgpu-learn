204278ea1dcaed266a4100a3879458a91fb0fbd3
Iliyan-Y <maximizebg@gmail.com>
2023-03-23 10:15:39 +0000

    render

--- src/window_runner.rs
+++ src/window_runner.rs
@@ -98,10 +98,16 @@ impl State {
   }
 
   fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-    todo!()
+    if new_size.width > 0 && new_size.height > 0 {
+      self.size = new_size;
+      self.config.width = new_size.width;
+      self.config.height = new_size.height;
+      self.surface.configure(&self.device, &self.config);
+    }
   }
 
   fn input(&mut self, event: &WindowEvent) -> bool {
+    // f the method returns true, the main loop won't process the event any further.
     // no event to capture yet
     false
   }
@@ -111,41 +117,42 @@ impl State {
   }
 
   fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-    // let output = self.surface.get_current_texture()?;
-    // let view = output
-    //   .texture
-    //   .create_view(&wgpu::TextureViewDescriptor::default());
+    let output = self.surface.get_current_texture()?;
+    let view = output
+      .texture
+      .create_view(&wgpu::TextureViewDescriptor::default());
 
-    // let mut encoder = self
-    //   .device
-    //   .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-    //     label: Some("Render Encoder"),
-    //   });
+    let mut encoder = self
+      .device
+      .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("Render Encoder"),
+      });
 
-    // {
-    //   let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-    //     label: Some("Render Pass"),
-    //     color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-    //       view: &view,
-    //       resolve_target: None,
-    //       ops: wgpu::Operations {
-    //         load: wgpu::LoadOp::Clear(wgpu::Color {
-    //           r: 0.1,
-    //           g: 0.2,
-    //           b: 0.3,
-    //           a: 1.0,
-    //         }),
-    //         store: true,
-    //       },
-    //     })],
-    //     depth_stencil_attachment: None,
-    //   });
-    // }
+    // the {} block borrows encoder mutably
+    {
+      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: Some("Render Pass"),
+        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+          view: &view,
+          resolve_target: None,
+          ops: wgpu::Operations {
+            load: wgpu::LoadOp::Clear(wgpu::Color {
+              r: 0.1,
+              g: 0.2,
+              b: 0.3,
+              a: 1.0,
+            }),
+            store: true,
+          },
+        })],
+        depth_stencil_attachment: None,
+      });
+    }
 
-    // self.queue.submit(std::iter::once(encoder.finish()));
-    // output.present();
+    self.queue.submit(std::iter::once(encoder.finish()));
+    output.present();
 
-    // Ok(())
+    Ok(())
   }
 }
 
@@ -176,13 +183,13 @@ pub async fn run() {
                 },
               ..
             } => *control_flow = ControlFlow::Exit,
-            // WindowEvent::Resized(physical_size) => {
-            //   state.resize(*physical_size);
-            // }
-            // WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-            //   // new_inner_size is &&mut so w have to dereference it twice
-            //   state.resize(**new_inner_size);
-            // }
+            WindowEvent::Resized(physical_size) => {
+              state.resize(*physical_size);
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+              // new_inner_size is &&mut so w have to dereference it twice
+              state.resize(**new_inner_size);
+            }
             _ => {}
           }
         }
